2963d0d42c4c207fc856d6ab2103128c902c20bc
Isaac Levine <isaacmlevine4@gmail.com>
2024-08-10 14:40:17 -0400

    Last challenge: Read request body

--- app/main.py
+++ app/main.py
@@ -1,80 +1,97 @@
-import socket
-from threading import Thread
+import asyncio
+import argparse
+import re
+import sys
+from asyncio.streams import StreamReader, StreamWriter
+from pathlib import Path
 
-def reply(req, code, body="", headers={}):
-    b_reply = b""
-    match code:
-        case 200:
-            b_reply += b"HTTP/1.1 200 OK\r\n"
-        case 404:
-            b_reply += b"HTTP/1.1 404 Not Found\r\n"
-        case 500:
-            b_reply += b"HTTP/1.1 500 No\r\n"
-    if not "Content-Type" in headers:
-        headers["Content-Type"] = "text/plain"
-    if body != "":
-        headers["Content-Length"] = str(len(body))
-    for key, val in headers.items():
-        b_reply += bytes(key, "utf-8") + b": " + bytes(val, "utf-8") + b"\r\n"
-    b_reply += b"\r\n" + bytes(body, "utf-8")
-    return b_reply
+GLOBALS = {}
 
-def handle_request(conn, req):
-    if req["path"] == "/":
-        return reply(req, 200)
-    if req["path"].startswith("/echo/"):
-        return reply(req, 200, req["path"][6:])
-    if req["path"] == "/user-agent":
-        ua = req["headers"]["User-Agent"]
-        return reply(req, 200, ua)
-    return reply(req, 404)
+def stderr(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
 
-def parse_request(bytes):
-    output = {"method": "", "path": "", "headers": {}, "body": ""}
-    lines = bytes.decode("utf-8").split("\r\n")
-    if len(lines) < 3:
-        return None
-    reqLine = lines[0].split(" ")
-    if (not reqLine[0]) or reqLine[0] not in ["GET", "POST", "PUT", "HEAD"]:
-        return None
-    if (not reqLine[1]) or reqLine[1][0] != "/":
-        return None
-    output["method"] = reqLine[0]
-    output["path"] = reqLine[1]
-    # Ignore HTTP version
-    lines = lines[1:]
-    c = 0
-    for l in lines:
-        if l == "":
-            break
-        headLine = l.split(":")
-        output["headers"][headLine[0]] = headLine[1].lstrip()
-        c += 1
-    output["body"] = lines[c + 1]
-    return output
+def parse_request(content: bytes) -> tuple[str, str, dict[str, str], str]:
+    first_line, *tail = content.split(b"\r\n")
+    method, path, _ = first_line.split(b" ")
+    headers: dict[str, str] = {}
+    while (line := tail.pop(0)) != b"":
+        key, value = line.split(b": ")
+        headers[key.decode()] = value.decode()
+    return method.decode(), path.decode(), headers, b"".join(tail).decode()
 
-def handle_client(conn):
-    byte = []
-    try:
-        while (byte := conn.recv(1024)) != b"":
-            parsed_req = parse_request(byte)
-            if parsed_req == None:
-                conn.send(str.encode("HTTP/1.1 500 No\r\n\r\n"))
-                return conn.close()
-            # Recv & parsed request
-            conn.send(handle_request(conn, parsed_req))
-            return conn.close()
-    except Exception as e:
-        print("handle_client err", e)
+def make_response(
+    status: int,
+    headers: dict[str, str] | None = None,
+    body: str = "",
+) -> bytes:
+    headers = headers or {}
+    msg = {
+        200: "OK",
+        201: "CREATED",
+        404: "NOT FOUND",
+    }
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    threads = []
-    while 1:
-        conn, addr = server_socket.accept()  # wait for client
-        t = Thread(target=handle_client, args=[conn])
-        threads.append(t)
-        t.run()
+    return b"\r\n".join(
+        map(
+            lambda i: i.encode(),
+            [
+                f"HTTP/1.1 {status} {msg[status]}",
+                *[f"{k}: {v}" for k, v in headers.items()],
+                f"Content-Length: {len(body)}",
+                "",
+                body,
+            ],
+        ),
+    )
+    
+async def handle_connection(reader: StreamReader, writer: StreamWriter) -> None:
+    method, path, headers, body = parse_request(await reader.read(2**16))
+    if re.fullmatch(r"/", path):
+        writer.write(b"HTTP/1.1 200 OK\r\n\r\n")
+        stderr(f"[OUT] /")
+    elif re.fullmatch(r"/user-agent", path):
+        ua = headers["User-Agent"]
+        writer.write(make_response(200, {"Content-Type": "text/plain"}, ua))
+        stderr(f"[OUT] user-agent {ua}")
+    elif match := re.fullmatch(r"/echo/(.+)", path):
+        msg = match.group(1)
+        writer.write(make_response(200, {"Content-Type": "text/plain"}, msg))
+        stderr(f"[OUT] echo {msg}")
+    elif match := re.fullmatch(r"/files/(.+)", path):
+        p = Path(GLOBALS["DIR"]) / match.group(1)
+        if method.upper() == "GET" and p.is_file():
+            writer.write(
+                make_response(
+                    200,
+                    {"Content-Type": "application/octet-stream"},
+                    p.read_text(),
+                )
+            )
+        elif method.upper() == "POST":
+            p.write_bytes(body.encode())
+            writer.write(make_response(201))
+        else:
+            writer.write(make_response(404))
+        stderr(f"[OUT] file {path}")
+    else:
+        writer.write(make_response(404, {}, ""))
+        stderr(f"[OUT] 404")
 
+    writer.close()
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", default=".")
+    args = parser.parse_args()
+
+    GLOBALS["DIR"] = args.directory
+
+    server = await asyncio.start_server(handle_connection, "localhost", 4221)
+    async with server:
+        stderr("Starting server...")
+        stderr(f"--directory {GLOBALS['DIR']}")
+        await server.serve_forever()
+
+        
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
